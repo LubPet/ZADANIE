b13bfe8fcfae9cd88079ab7b576f8591c3595185
Libor Sestrienka <sestrienka@netgrif.com>
2019-12-01 17:28:31 +0100

    - logout - logged user info - 3

--- app/src/main/java/com/example/mobv/LoginActivity.kt
+++ app/src/main/java/com/example/mobv/LoginActivity.kt
@@ -42,6 +42,7 @@ class LoginActivity : AppCompatActivity() {
                         if (task.isSuccessful) {
                             val intent = Intent(this@LoginActivity, MainActivity::class.java)
                             intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+
                             startActivity(intent)
                             finish()
                         } else {
--- app/src/main/java/com/example/mobv/StartActivity.java
+++ app/src/main/java/com/example/mobv/StartActivity.java
@@ -7,9 +7,28 @@ import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class StartActivity extends AppCompatActivity {
 
     Button login, register;
+
+    FirebaseUser firebaseUser;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        if (firebaseUser != null){
+            Intent intent = new Intent(StartActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+
+    }
 
     @Override
     protected void onCreate(Bundle saveInstanceState) {
