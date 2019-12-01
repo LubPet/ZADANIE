328707e1a2b1174ffc40d0e42f5a9811a0e06c13
petrovic.lubos <petrovic@netgrif.com>
2019-12-01 11:48:48 +0100

    Registrácia

--- app/src/main/java/com/example/mobv/MainActivity.kt
+++ app/src/main/java/com/example/mobv/MainActivity.kt
@@ -3,10 +3,10 @@ package com.example.mobv
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class RegisterActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        setContentView(R.layout.activity_main)
     }
 }
---
+++ app/src/test/java/com/example/mobv/RegisterActivityTest.java
@@ -0,0 +1,7 @@
+package com.example.mobv;
+
+import static org.junit.Assert.*;
+
+public class RegisterActivityTest {
+
+}
