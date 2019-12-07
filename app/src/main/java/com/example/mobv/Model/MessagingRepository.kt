261263d427cf1fa5783ff741e363c19f2696a908
Timotej Bezák <bezak@netgrif.com>
2019-12-07 17:51:56 +0100

    - minor improvement in messaging repo

--- app/src/main/java/com/example/mobv/Model/MessagingRepository.kt
+++ app/src/main/java/com/example/mobv/Model/MessagingRepository.kt
@@ -12,15 +12,19 @@ import java.lang.Exception
 class MessagingRepository: IMessagingRepository {
 
     override fun getContacts(ctx: Context, uid: String, onSuccess: (List<Contact>) -> Unit, onFailure: (Throwable) -> Unit) {
-        val response: Call<List<Contact>> = ZadanieApi.create(ctx).listContacts(ListContactRequest(uid))
+        val call: Call<List<Contact>> = ZadanieApi.create(ctx).listContacts(ListContactRequest(uid))
 
-        return response.enqueue(object: Callback<List<Contact>> {
+        fetch(call, onSuccess, onFailure)
+    }
 
-            override fun onFailure(call: Call<List<Contact>>, t: Throwable) {
+    private fun <T> fetch(call: Call<T>, onSuccess: (T) -> Unit, onFailure: (Throwable) -> Unit) {
+        return call.enqueue(object: Callback<T> {
+
+            override fun onFailure(call: Call<T>, t: Throwable) {
                 onFailure(t)
             }
 
-            override fun onResponse(call: Call<List<Contact>>, response: Response<List<Contact>>) {
+            override fun onResponse(call: Call<T>, response: Response<T>) {
                 if (response.code() == 200) {
                     onSuccess(response.body()!!)
 
