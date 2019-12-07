35654d1d04c82c5f2904415864e0aa2830c9e7fa
Libor Sestrienka <sestrienka@netgrif.com>
2019-12-07 16:21:19 +0100

    MainActivity.java na MainActivity.kt

---
+++ app/src/main/java/com/example/mobv/MainActivity.kt
@@ -0,0 +1,111 @@
+package com.example.mobv
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import androidx.viewpager.widget.ViewPager
+
+import com.example.mobv.Fragments.ChatsFragment
+import com.example.mobv.Fragments.RoomFragment
+import com.example.mobv.Fragments.UsersFragment
+import com.example.mobv.Model.FirebaseIdRepository
+import com.example.mobv.Model.UserRepository
+import com.example.mobv.Model.User
+import com.google.android.material.tabs.TabLayout
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+
+import java.util.ArrayList
+
+import de.hdodenhof.circleimageview.CircleImageView
+
+class MainActivity : AppCompatActivity() {
+
+//    internal var profile_image: CircleImageView
+//    internal var username: TextView
+
+    internal var userRepository = UserRepository()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar!!.title = ""
+
+        var profile_image: CircleImageView = findViewById(R.id.profile_image)
+        var username: TextView = findViewById(R.id.usernameMain)
+
+        val tabLayout = findViewById<TabLayout>(R.id.tab_layout)
+        val viewPager = findViewById<ViewPager>(R.id.view_pager)
+
+        val viewPagerAdapter = ViewPagerAdapter(supportFragmentManager)
+
+        viewPagerAdapter.addFragment(RoomFragment(), "Roomka")
+        viewPagerAdapter.addFragment(UsersFragment(), "Kontakty")
+        viewPagerAdapter.addFragment(ChatsFragment(), "Konverzácie")
+
+        viewPager.adapter = viewPagerAdapter
+        tabLayout.setupWithViewPager(viewPager)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+
+            R.id.logout -> {
+                userRepository.logout(this@MainActivity)
+                startActivity(Intent(this@MainActivity, StartActivity::class.java))
+                finish()
+                return true
+            }
+        }
+
+        return false
+    }
+
+    internal inner class ViewPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+
+        private val fragments: ArrayList<Fragment>
+        private val titles: ArrayList<String>
+
+        init {
+            this.fragments = ArrayList()
+            this.titles = ArrayList()
+        }
+
+        override fun getItem(position: Int): Fragment {
+            return fragments[position]
+        }
+
+        override fun getCount(): Int {
+            return fragments.size
+        }
+
+        fun addFragment(fragment: Fragment, title: String) {
+            fragments.add(fragment)
+            titles.add(title)
+        }
+
+        override fun getPageTitle(position: Int): CharSequence? {
+            return titles[position]
+        }
+    }
+}
